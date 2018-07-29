6b404b9383676333a073108c595897d50c334183
Wojciech Zarski <wojciech.zarski@gmail.com>
2018-07-29 22:33:04 +0200

    Refactor movie-list to APIView
    APIView gives more controll over handling methods
    so it's suppose to be easier to customize more complex
    behaviours.

--- moviesdb/urls.py
+++ moviesdb/urls.py
@@ -1,15 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 
 from moviesdbapi import views
 
-router = routers.DefaultRouter()
-router.register(r'movies', views.MovieViewSet)
-
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^movies/', views.MovieList.as_view(), name="movie-list"),
     path('admin/', admin.site.urls),
     url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
