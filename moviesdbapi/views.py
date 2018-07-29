34d426d5f8f6e0c2178498841a33658c2314d189
Wojciech Zarski <wojciech.zarski@gmail.com>
2018-07-29 23:10:18 +0200

    Add saving new movies

--- moviesdbapi/views.py
+++ moviesdbapi/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -14,3 +15,10 @@ class MovieList(APIView):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
