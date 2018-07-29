eb579fc0c32d6bc2cc047a9a9af6feb06ff79c1f
Wojciech Zarski <wojciech.zarski@gmail.com>
2018-07-29 17:04:17 +0200

    Add gunicorn

--- moviesdb/settings.py
+++ moviesdb/settings.py
@@ -120,8 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
